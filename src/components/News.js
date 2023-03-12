6431e010e3b70eb99e892ced45e3f510848309a9
omrajindra <omrajindra.ori@gmail.com>
2023-03-12 19:35:39 +0545

    3.Fetching API key

--- src/components/News.js
+++ src/components/News.js
@@ -4,12 +4,19 @@ import NewsItem from "./NewsItem";
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is news
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className="container my-3">
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="mytitle" desciption="myDescription" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" desciption="myDescription" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" desciption="myDescription" />
+          </div>
+        </div>
       </div>
     );
   }
